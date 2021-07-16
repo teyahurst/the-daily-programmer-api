200f38b6afd1fbe974ccc9d9b87f6d4b68a3e4b9
Teya Hurst <teyahurst@gmail.com>
2021-07-16 09:45:28 -0500

    added cors support

--- src/app.js
+++ src/app.js
@@ -1,34 +1,25 @@
-require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
-const helmet = require('helmet');
 
-const { NODE_ENV } = require('./config')
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+)
+
+
+
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+const PORT = process.env.PORT || 3000;
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.get('/api/*', (req, res) => {
+    res.json({ok: true});
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`))
 
-app.use(function errorHandler(error, req, res, next){
-    let response 
-    if(NODE_ENV === 'production'){
-        response = { error: { message: 'server error'}}
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
-
-module.exports = app;
+module.exports = {app};
