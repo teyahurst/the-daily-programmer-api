1e8e5c7e68b8ab4cac928c9c980c77ad20e662c9
Teya Hurst <teyahurst@gmail.com>
2021-07-26 21:55:31 -0500

    added news api config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const app = require('express')();
 const cors = require('cors');
+const proxy = require('express-http-proxy');
+const fetch = require('node-fetch')
+const NewsApi = require('newsapi')
 
-const {CLIENT_ORIGIN} = require('./config');
+
+
+const {CLIENT_ORIGIN, API_KEY} = require('./config');
+
 
 app.use(
     cors({
@@ -11,14 +17,43 @@ app.use(
 
 
 
+const newsapi = new NewsApi('a5b0fa76d282400d8c21da799e95fcd6');
 
-const app = express();
+app.get('/news', (req, res) => {
+    newsapi.v2.everything({
+        q: 'software-engineering',
+        pageSize: 10,
+        language: 'en',
+    })
+    .then(response => {
+        console.log(response)
+    })
+
+})
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
+app.get('/api/news', (req, res) => {
+    res.send('News')
+    
+
+
+
+    
+})
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/api/*', (req, res) => {
-    res.json({ok: true});
-});
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`))
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+    BASE_URL: process.env.API_URL,
+    API_KEY: process.env.API_KEY
 }
